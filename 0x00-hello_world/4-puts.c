ddc79177fa8ae47263f5f591695d3d24f0b79b08
yibe1 <yibe1481@gmail.com>
2022-06-30 23:39:33 +0300

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/*c with betty style*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts('"Programming is like building a multilingual puzzle\n');
 return (0);
 }
