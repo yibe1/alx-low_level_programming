cbe8263e10f27950099c95f723650082707d90cc
yibe1 <yibe1481@gmail.com>
2022-07-01 22:59:52 +0300

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main-start of function
+ *
+ * Description: this function prints numbers
+ *
+ * Return: returns 0 for success
+ *
+ */
+
+int main(void)
+{
+  int i;
+  for (i = 0; i < 11; i++)
+    {
+      putchar(i + '0');
+    }
+  return (0);
+}
