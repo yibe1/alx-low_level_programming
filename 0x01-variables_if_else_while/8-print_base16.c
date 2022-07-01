2692d81bf91a366c004299a8cbd54b85d3668af5
yibe1 <yibe1481@gmail.com>
2022-07-01 23:49:08 +0300

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,9 +12,9 @@
 int main(void)
 {
 int i;
-for( i = 0; i < 10; i++ )
+for (i = 0; i < 10; i++)
 putchar(i + '0');
-for( i = 'a'; i < 'g'; i++ )
+for (i = 'a'; i < 'g'; i++)
 putchar(i);
 putchar('\n');
 return (0);
