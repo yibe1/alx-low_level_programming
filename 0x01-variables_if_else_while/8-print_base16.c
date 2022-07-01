1881a4097cebbfa3941b2e959574c5238e378054
yibe1 <yibe1481@gmail.com>
2022-07-01 23:45:55 +0300

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-start of function
+ *
+ * Description: this program prints hexadecimal
+ *
+ * Retrun: returns 0 for success
+ *
+ */
+
+int main(void)
+{
+int i;
+for(i = 0; i<10; i++)
+putchar(i + '0');
+for(i = 'a'; i < 'g'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
