a213448833905aded263b8a2c951ce758ada3be0
yibe1 <yibe1481@gmail.com>
2022-07-02 00:56:48 +0300

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-int i, j, k, l, m;
+int i;
 for (i = 0; i < 10; i++)
-for(j = 0; j < 10; j++)
-for(k = 0; k < 10; k++)
-for(l = 0; l < 10; l++)
-for(m = 0; m < 10; m++)
+{
 putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
 return (0);
 }
