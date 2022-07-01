2040dd2339a94cfdf109f819db7bcd6ad5426af3
yibe1 <yibe1481@gmail.com>
2022-07-01 23:10:16 +0300

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-start of function
+ *
+ * Description: this program prints alphabets in reverse order
+ *
+ * Return: 0 for success
+ *
+ */
+
+int main(void)
+{
+int i;
+ char txt[] = "\nabcdefghijklmnopqrstuvwx";
+ 
+for (i = 26; i < 0; i--)
+{
+putchar(txt[i]);
+}
+return (0);
+}
