126ebce496f8ad270f52fbc6215b9edd0133aafa
yibe1 <yibe1481@gmail.com>
2022-07-01 19:04:07 +0300

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-the first fxn in the program
+ *
+ * Description: this program prints 0-9
+ *
+ * Return: 0 for success
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for( i = 0; i < 0; i++ )
+	{
+		printf(i);
+	}
+
+	return (0);
+}
