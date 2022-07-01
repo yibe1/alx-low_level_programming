d2b4c7e5b2b446439ff15e1b6031c57364800292
yibe1 <yibe1481@gmail.com>
2022-07-01 17:48:28 +0300

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main-is the entry function
+ *
+ * Description: this is a function that converts alphabets to lowercase
+ *
+ * Return: returns 0 for success
+ *
+ */
+
+int main()
+{
+	string txt = "abcdefghijklmnopqrstuvwxyz";
+	for(int i = 0; i<26; i++)
+	{
+		putchar(txt[i]);
+	}
+
+	return 0;
+}
