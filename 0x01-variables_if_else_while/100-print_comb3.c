2d669f7c32c5348148c6f708e5ff7d2c6bc13157
yibe1 <yibe1481@gmail.com>
2022-07-03 21:41:04 +0300

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main-starting point
+ *
+ * Description: program that prints combination of two digits
+ *
+ * Return: returns 0 for success
+ *
+ */
+
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = i; j<10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if(i > 9)
+break;
+putchar(',');
+putchar(' ');
+     
+}
+}
+return (0);
+}
+      
