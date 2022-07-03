b58dddfd010eb73412fa56d939c4e4c9a6ea08e1
yibe1 <yibe1481@gmail.com>
2022-07-04 00:17:50 +0300

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,28 +14,24 @@ int main(void)
 int i, j, k, l;
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j <=10; j++)
+for (j = 0; j <= 10; j++)
 {
 for (k = i; k <= 10; k++)
 {
 for (l = j + 1; l <= 10; l++)
 {
-if(j == 10){
+if (j == 10){
 j = 0;
 i++;
 }
-if (k == 9 && l == 10)
+if (l == 10)
 {
 k = 0;
-j++;
-}  
+k++;
+}
 putchar(i + '0');
 putchar(j + '0');
 putchar(' ');
-if (l == 10 && k != 9){
-l = 0;
-k++;
-}
 putchar(k + '0');
 putchar(l + '0');
 if (i == 9 && j == 7 && k == 9 && l == 8)
