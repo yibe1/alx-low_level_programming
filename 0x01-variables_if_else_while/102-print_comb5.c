8ece9cfd20f3b6a2f0df8ce3ee5b41eb6d4d1318
yibe1 <yibe1481@gmail.com>
2022-07-03 23:20:10 +0300

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,9 +16,9 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-for (k = i; k <10; k++)
+for (k = i; k < 10; k++)
 {
-for(l = j + 1; l < 10; l++)
+for(l = j + 1; l <= 10; l++)
 {
 putchar(i + '0');
 putchar(j + '0');
