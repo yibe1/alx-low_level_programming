19b8ea71723cc26a812e9589e6b54a3e72b18ece
yibe1 <yibe1481@gmail.com>
2022-07-05 17:43:41 +0300

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 /**
  * _islower-function that checks for lowercase
- *
+ *@c: the character to be checked
  * Description: the function checks for small letter
  *
  * Return: return 1 if c is lowercase or 0 if not
