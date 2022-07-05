5cca545fe3a14c21b8fe620be0d77a50f307d892
yibe1 <yibe1481@gmail.com>
2022-07-05 18:13:45 +0300

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_sign - prints the function that returns the result
  *
@@ -12,17 +13,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+putchar('+');
 return (1);
 }
 if (n == 0)
 {
-_putchar('0');
+putchar('0');
 return (0);
 }
 if (n < 0)
 {
-_putchar('-');
+putchar('-');
 return (-1);
 }
 }
