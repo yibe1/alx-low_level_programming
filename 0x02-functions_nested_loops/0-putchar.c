fb8e0177c2c3405f47d9df355425102000559bb0
yibe1 <yibe1481@gmail.com>
2022-07-05 11:50:16 +0300

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main-start of the function
+ *
+ * Description: program that prints _putchar
+ *
+ * Return: returns 0 for success
+ *
+ */
+
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
