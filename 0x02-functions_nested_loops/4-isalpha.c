0f879774d0cbeecc5c84f26a0deaa09ded49c081
yibe1 <yibe1481@gmail.com>
2022-07-05 17:48:51 +0300

    update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+/**
+ * _islower-function that checks for lowercase
+ *@c: the character to be checked
+ * Description: the function checks for small letter
+ *
+ * Return: return 1 if c is a letter and alphabet or 0 if not
+ *
+ */
+
+
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+return (0);
+}
