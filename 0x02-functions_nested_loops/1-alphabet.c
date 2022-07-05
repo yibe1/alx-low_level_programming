2edf7a7c0b1c5140c5041c1306a105b0eb8acbae
yibe1 <yibe1481@gmail.com>
2022-07-05 16:38:54 +0300

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h>
+
+/**
+ * main-start of program
+ *
+ * Description: using prototypes
+ *
+ * Return: return 0 for success
+ *
+ */
+
+void print_alphabet(void);
+int main(void)
+{
+print_alphabet();
+return 0;
+}
+
+/**
+ * print_alphabet-start of program
+ *
+ * Description: using prototypes
+ *
+ * Return: nothing
+ *
+ */
+void print_alphabet(void)
+{
+int i;
+char ch[] = "abcdefghijklmnopqrstuvwxyz\n";
+for (i = 0; i < 27; i++)
+{
+putchar(ch[i]);
+}
+}
