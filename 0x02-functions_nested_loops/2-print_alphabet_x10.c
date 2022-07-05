63b4aba653b9ca8233c626c69d6b1750d25dc917
yibe1 <yibe1481@gmail.com>
2022-07-05 17:20:58 +0300

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 {
 int i, j;
 char ch[] = "abcdefghijklmnopqrstuvwxyz\n";
-for(j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 for (i = 0; i < 27; i++)
 {
 putchar(ch[i]);
