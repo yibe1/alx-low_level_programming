7dbdd337b7b7fec58ab252b197354dc3a47b8754
yibe1 <yibe1481@gmail.com>
2022-07-05 17:32:54 +0300

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower-function that checks for lowercase
+ *
+ * Return: return 1 if c is lowercase or 0 if not
+ *
+ */
+
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+} else
+return (0);
+}
