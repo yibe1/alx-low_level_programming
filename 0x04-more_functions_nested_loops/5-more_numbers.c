d61240fa769f1e90d2f400c1ec9f67acc345835a
yibe1 <yibe1481@gmail.com>
2022-07-07 23:34:21 +0300

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers-function that checks for lowercase
+ *
+ * Description: the function checks for small letter
+ *
+ * Return: ..
+ *
+ */
+
+
+void more_numbers(void)
+{
+int i = 0, j;
+while (i < 10)
+{
+for (j = 0; j < 15; j++)
+{
+_putchar(j + '0');
+j++
+}
+i++;
+}
+_putchar('\n');
+}
