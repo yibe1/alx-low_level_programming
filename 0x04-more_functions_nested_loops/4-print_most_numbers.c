599afad9bf1ba57bc10370a4fa93beb576964fc9
yibe1 <yibe1481@gmail.com>
2022-07-07 23:28:08 +0300

    update

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers-function that checks for lowercase
+ *
+ * Description: the function checks for small letter
+ *
+ * Return: ..
+ *
+ */
+
+
+void print_most_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+if (i == 2 || i == 4)
+{
+i++;
+contineu;
+}
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
