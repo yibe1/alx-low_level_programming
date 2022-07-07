cd49aae0e8415ecf4964f6ed22225bee5fc72f64
yibe1 <yibe1481@gmail.com>
2022-07-07 23:13:39 +0300

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul-function that checks for lowercase
+ *@a: the first number
+ *@b: the second number
+ * Description: the function checks for small letter
+ *
+ * Return: return 1 if c is lowercase or 0 if not
+ *
+ */
+
+
+int mul(int a, int b)
+{
+
+return (a * b);
+}
