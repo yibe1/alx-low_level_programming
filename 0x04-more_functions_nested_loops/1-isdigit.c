dbe312cf7d3dc617b1a03b2282f7e5e2f27d1478
yibe1 <yibe1481@gmail.com>
2022-07-07 23:05:18 +0300

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+/**
+ * _isdigit-function that checks for lowercase
+ *@c: the character to be checked
+ * Description: the function checks for small letter
+ *
+ * Return: return 1 if c is lowercase or 0 if not
+ *
+ */
+
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+return (0);
+}
