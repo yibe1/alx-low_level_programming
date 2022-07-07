ae1b6c9a3a5edc5f81c29e097fb0e636bffc20ce
yibe1 <yibe1481@gmail.com>
2022-07-07 23:19:18 +0300

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numbers-function that checks for lowercase
+ *
+ * Description: the function checks for small letter
+ *
+ * Return: ..
+ *
+ */
+
+
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+_putchar(i + '0');
+_putchar('\n');
+}
