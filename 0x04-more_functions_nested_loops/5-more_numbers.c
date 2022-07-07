340b38356835573db9027ac172e4800653329174
yibe1 <yibe1481@gmail.com>
2022-07-08 00:15:21 +0300

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,7 @@ void more_numbers(void)
 {
 int i = 0;
 int j, t2;
- 
+
 while (i < 10)
 {
 for (j = 0; j < 15; j++)
@@ -21,7 +21,6 @@ for (j = 0; j < 15; j++)
 if (j > 10)
 j = j / 10;
 _putchar(j + '0');
-  
 if (j > 9)
 {
 t2 = j % 10;
