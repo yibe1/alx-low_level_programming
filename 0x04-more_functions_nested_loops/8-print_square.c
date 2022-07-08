0f28abaccbac313be29c7f33f766ac07949aa25e
yibe1 <yibe1481@gmail.com>
2022-07-08 10:44:34 +0300

    update

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square-function to print triangle
+ *
+ * Description: ...
+ * @size: size of triangle
+ *
+ * Return: nothing
+ *
+ */
+
+void print_square(int size)
+{
+int i, k;
+for (i = 0; i < size; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
