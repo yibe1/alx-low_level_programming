47312e05dd709de2105c0831366ef097f94ea027
yibe1 <yibe1481@gmail.com>
2022-07-08 09:46:41 +0300

    update

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line-function to print triangle
+ *
+ * Description: ...
+ * @n: size of triangle
+ *
+ * Return: nothing
+ *
+ */
+
+void print_line(int n)
+{
+for (k = 0; k < n; k++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
