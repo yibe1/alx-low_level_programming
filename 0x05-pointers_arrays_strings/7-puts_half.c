3e93c6e7acd9d571cfc9094bfa4cb64f8ff54f94
yibe1 <yibe1481@gmail.com>
2022-07-12 02:24:32 +0300

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts2-str length
+ * @str:first char
+ *
+ * Return: return string length
+ *
+ */
+
+void puts2(char *str)
+{
+int i = 0, n, i = 0;
+ 
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+n = i / 2;
+}
+else
+{
+n = (i - 1) / 2;
+}
+while (n < i)
+{
+_putchar(str[n]);
+n++;
+}
+_putchar('\n');
+}
