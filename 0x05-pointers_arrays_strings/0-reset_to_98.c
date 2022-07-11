07df50ec0d41e998ef10632690321eb6ead8f297
yibe1 <yibe1481@gmail.com>
2022-07-11 22:58:06 +0300

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98-resets number
+ * @n: input
+ *
+ * Return: nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+void _putchar(char r);
+void print_line(int n);
+#endif
