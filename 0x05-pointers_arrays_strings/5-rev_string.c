b4ff619b987e047846c7af7d9e6b61214446a701
yibe1 <yibe1481@gmail.com>
2022-07-12 00:22:38 +0300

    update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,13 +13,19 @@ void rev_string(char *str)
 int n = 0;
 while (str[n] != '\0')
 {
-_putchar(str[n]);
 n++;
 }
-_putchar('\n');
+char ch[n];
+int i = 0;
+while (i < n)
+{
+ch[i] = str[i];
+i++;
+}
+
 while (n > 0)
 {
-_putchar(str[n - 1]);
+*str = ch[n];
 n--;
 }
 _putchar('\n');
