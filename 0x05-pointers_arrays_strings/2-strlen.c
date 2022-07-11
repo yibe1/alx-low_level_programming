b3fd3dc6605893102cd7f33b1f18db61335fa0b5
yibe1 <yibe1481@gmail.com>
2022-07-11 23:29:09 +0300

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen-str length
+ * @s:first char
+ *
+ * Return: return string length
+ *
+ */
+
+int _strlen(char *s)
+{
+int n;
+while (true)
+{
+if (arr(*s + n) == '\0')
+break;
+else
+n++;
+}
+return (n);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c~
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int-swaps integers
+ * @a:number1
+ * @b:number2
+ *
+ * Return: returns nothing
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
