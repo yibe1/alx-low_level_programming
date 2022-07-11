46f06e3a644a23b5028ab84442115411713e8b37
yibe1 <yibe1481@gmail.com>
2022-07-12 02:12:41 +0300

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,31 +1,24 @@
 #include "main.h"
 
 /**
- * rev_string-str length
+ * puts2-str length
  * @str:first char
  *
  * Return: return string length
  *
  */
 
-void rev_string(char *str)
+void puts2(char *str)
 {
 int n = 0, j;
 int i = 0, c;
 char t;
 while (str[n] != '\0')
 {
+if (n % 2 != 0)
+continue;
+_putchar(str[n]);  
 n++;
 }
-j = n - 1;
-c = j / 2;
-while (i < c)
-{
-t = str[j];
-str[j] = str[i];
-str[i] = t;
-j--;
-i++;
-}
-str[n] = '\0';
+_putchar('\n');
 }
