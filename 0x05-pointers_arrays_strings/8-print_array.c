731fef4c05dae54900af99f34441c4fb9e7eac02
yibe1 <yibe1481@gmail.com>
2022-07-12 03:01:15 +0300

    update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * print_array-str length
+ * @str:first char
+ * @n:n to be printed
+ *
+ * Return: return string length
+ *
+ */
+
+void print_array(char *str, int n)
+{
+int i = 1;
+printf(str[0]);
+while (str[i] != '\0')
+{
+if (i == n)
+break;
+printf(", ");
+printf("%d", str[i]);
+i++;
+}
+}
