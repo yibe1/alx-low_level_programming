01313cbfd5d5b84b022ccb5d889f4a068f051f09
yibe1 <yibe1481@gmail.com>
2022-07-12 03:44:52 +0300

    update

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy-copies char
+ * @dest: dest to be copied
+ * @src: source
+ *
+ * Return: return pointer
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while(src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (*dest);
+}
