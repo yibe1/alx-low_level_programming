e296e9decc46610348cc3f15aedcb9c981a556f9
yibe1 <yibe1481@gmail.com>
2022-07-14 01:27:54 +0300

    UpdateA

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,23 +11,22 @@ int _strcmp(char *s1, char *s2)
 {
 int j = 0;
 int c1 = 0, c2 = 0;
-while (s1[j] != '\0' || s2[j] != '\0')
-{
-if (s1[j] != '\0')
+while (s1[j] != '\0')
 {
 c1 = c1 + s1[j];
-}
-
-if (s2[j] != '\0')
-{
-c2 = c2 + s2[j];
-}
 j++;
 }
+j = 0;
+while (s2[j] != '\0')
+{
+c2 = c2 + s2[j];
+j++;
+}
+
 if (c1 > c2)
-return 1;
+return (1);
 else if (c1 == c2)
-return 0;
+return (0);
 else
 return (-1);
 }
