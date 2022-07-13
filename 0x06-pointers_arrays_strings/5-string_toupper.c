9a80efe08b9eb1d236e110a4cd8791a9edcdb784
yibe1 <yibe1481@gmail.com>
2022-07-14 02:13:48 +0300

    Update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ * string_toupper-concatenating strings
+ *@a: var1
+ *
+ * Return: char
+ *
+ */
+
+char *string_toupper(char *a)
+{
+int i = 0;
+while (a[i] != '\0')
+{
+if (a[i] >= 97 && a[i] =< 122)
+{
+a[i] = a[i] - 32;
+}
+ 
+i++;
+}
+return (a);
+}
