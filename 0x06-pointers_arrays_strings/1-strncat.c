c8edbbe95f7b83ace42950830ec0225d88edb027
yibe1 <yibe1481@gmail.com>
2022-07-13 23:56:39 +0300

    Update

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+/**
+ * _strncat-concatenating strings
+ *@dest: var1
+ *@src:var2
+ *@n:amount
+ *
+ * Return: char
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+while (1 == 1)
+{
+if (dest[i] == '\0')
+{
+while (j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+break;
+}
+else
+{
+i++;
+}
+}
+return (dest);
+}
