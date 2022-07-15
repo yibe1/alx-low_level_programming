b903e5d2d38d1e22799be24058f72edddb733056
yibe1 <yibe1481@gmail.com>
2022-07-15 22:50:19 +0300

    update~

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  *print_number-prints number
  *@n:number to be printed
@@ -20,7 +20,7 @@ while (i > 1)
 {
 j = n / i;
 n = n % i;
-_putchar(j + '0');
+putchar(j + '0');
 i = i / 10;
 }
 putchar(n + '0');
