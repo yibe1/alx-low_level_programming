017aa0c65c2d07efaec2c2bc359b9ce4f8137013
yibe1 <yibe1481@gmail.com>
2022-07-18 22:44:40 +0300

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_memset-sets memory
+ *@s:char
+ *@b:char
+ *@n:size
+ *
+ *Return: char
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
+
+}
