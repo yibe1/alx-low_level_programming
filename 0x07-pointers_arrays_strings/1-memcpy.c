d53793b19d183eef870adeee2f4032ff3eda9888
yibe1 <yibe1481@gmail.com>
2022-07-19 20:21:41 +0300

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ *_memcpy-copy to memory
+ *@dest:destination variable
+ *@src:variable to be copied
+ *@n:the amount to be copied
+ *
+ *Return: return 0
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+for(i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
