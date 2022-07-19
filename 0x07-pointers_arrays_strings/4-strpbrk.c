9ed4b028445ff1c33b5a2ba28675220471a5438f
yibe1 <yibe1481@gmail.com>
2022-07-19 21:34:18 +0300

    update

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strpbrk - ............
+ *
+ * @s: .....
+ * @accept: ..........
+ *
+ * Return: a 
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return (NULL);
+}
