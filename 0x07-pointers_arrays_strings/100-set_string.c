07e167e5ad9bcca5eba39dfdf1ff7729f1df3717
yibe1 <yibe1481@gmail.com>
2022-07-19 21:58:05 +0300

    update

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,30 +1,14 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * print_diagsums -.......
+ * set_string - .....
  *
- * @a:..
- * @size:...
+ * @s: .........
+ * @to: .............
  *
  * Return: nothing
  */
 
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-int i = 0;
-int j = size - 1;
-int sum1 = 0;
-int sum2 = 0;
-while (i <= (size * size))
-{
-sum1 = sum1 + a[i];
-i = i + size + 1;
-}
-while (j < (size * size - 1))
-{
-sum2 += a[j];
-j = j + size - 1;
-}
-printf("%d, %d\n", sum1, sum2);
+*s = to;
 }
