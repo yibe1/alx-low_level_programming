d56da8d0f0ae1c92fe7953a7976678bbfcd3c725
yibe1 <yibe1481@gmail.com>
2022-07-20 23:16:11 +0300

    Update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * test -...........
+ * @f:..........
+ * @p:..............
+ *
+ * Return:.............
+ */
+int test(int f, int p)
+{
+if (p < 2 || p % f == 0)
+return (0);
+else if (f > p / 2)
+return (1);
+else
+return (test(f + 1, p));
+}
+
+/**
+ * is_prime_number -.........
+ * @n:............
+ *
+ * Return:............
+ */
+int is_prime_number(int n)
+{
+if (n == 2)
+return (1);
+}
