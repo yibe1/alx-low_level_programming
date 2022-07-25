3ab8209f5c291670a389f1e8ce908d50c46415b6
yibe1 <yibe1481@gmail.com>
2022-07-25 15:49:01 +0300

    Update

---
+++ 0x09-static_libraries/file1.c
@@ -0,0 +1,297 @@
+/**
+*_putchar-function that _putchar
+*@c:....
+
+*Return: nothing or some value
+*
+*/
+
+
+int _putchar(char c)
+
+{
+
+
+return (0);
+
+}
+
+/**
+*_islower-function that _islower
+*Return: nothing or some value
+*
+*/
+
+int _islower(int c)
+{
+
+return (0);
+
+}
+
+/**
+*_isalpha-function that _isalpha
+*@c:.....
+*Return: nothing or some value
+*
+*/
+
+int _isalpha(int c)
+{
+
+
+return (0);
+
+}
+
+/**
+*_abs-function that _abs
+*@n:.....
+*Return: nothing or some value
+*
+*/
+
+
+int _abs(int n)
+{
+
+return (0);
+
+}
+
+/**
+*_isupper-function that _isupper
+*@c:....
+*Return: nothing or some value
+*
+*/
+
+int _isupper(int c)
+{
+
+return (0);
+
+}
+
+/**
+*_isdigit-function that _isdigit
+*
+*Return: nothing or some value
+*
+*/
+
+int _isdigit(int c)
+{
+
+return (0);
+}
+
+/**
+*_strlen-function that _strlen
+*
+*@*
+*
+*Return: nothing or some value
+*
+*
+*/
+
+int _strlen(char *s)
+{
+
+return (0);
+}
+
+/**
+*_puts-function that _puts
+*
+*@*
+*Return: nothing or some value
+*
+*/
+
+void _puts(char *s)
+{
+
+}
+
+/**
+**_strcpy-function that *_strcpy
+*
+*@*dest:............
+*@*sr:..........
+*
+*Return: nothing or some value
+*
+*
+*/
+
+
+char *_strcpy(char *dest, char *src)
+{
+
+return (0);
+
+}
+
+/**
+*_atoi-function that _atoi
+*@*
+*Return: nothing or some value
+*
+*/
+
+
+int _atoi(char *s)
+{
+
+return (0);
+
+}
+
+/**
+**_strcat-function that *_strcat
+*@*dest,
+*@*sr
+*Return: nothing or some value
+*
+*/
+
+char *_strcat(char *dest, char *src)
+{
+
+return (0);
+}
+
+/**
+**_strncat-function that *_strncat
+*@*dest,
+*@*src,
+*Return: nothing or some value
+*
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+return (0);
+}
+
+/**
+**_strncpy-function that *_strncpy
+*@*dest,
+*@*src,
+*Return: nothing or some value
+*
+*/
+
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+return (0);
+
+}
+
+/**
+*_strcmp-function that _strcmp
+*@*s1,
+*@*s
+*Return: nothing or some value
+*
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+return (0);
+
+}
+
+/**
+**_memset-function that *_memset
+*@*s:......
+*@b:........
+*@n:........
+*Return: nothing or some value
+*
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+return (0);
+}
+
+/**
+**_memcpy-function that *_memcpy
+*@*dest:....
+*@*src:...
+*@n:....
+*Return: nothing or some value
+*
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+return (0);
+
+}
+
+/**
+**_strchr-function that *_strchr
+*@c:.......
+**s:.......
+*Return: nothing or some value
+*
+*/
+
+char *_strchr(char *s, char c)
+{
+
+return (0);
+
+}
+
+/**
+*_strspn-function that int _strspn
+*@*s:.......
+*@*accept...........
+*Return: nothing or some value
+*
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+return (0);
+
+}
+
+/**
+**_strpbrk-function that *_strpbrk
+*@*s:......
+*@*accept:.......
+*Return: nothing or some value
+*
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+
+return (0);
+
+}
+
+/**
+**_strstr-function that *_strstr
+*@*haystack:............
+*@*needle:..........
+*Return: nothing or some value
+*
+*/
+char *_strstr(char *haystack, char *needle)
+{
+return (0);
+}
