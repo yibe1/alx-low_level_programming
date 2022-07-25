0fef13adbd33fc2fbd01d17e3e630f23690b002b
yibe1 <yibe1481@gmail.com>
2022-07-25 18:38:35 +0300

    Update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: arg count
+ * @argv: arg value
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+
+return (0);
+}
