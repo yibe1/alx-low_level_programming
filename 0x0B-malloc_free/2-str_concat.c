c7ac9fba8e9a5549cbaa38da1bcc8f56e1b4ee17
yibe1 <yibe1481@gmail.com>
2022-07-25 23:18:42 +0300

    update

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len, a;
+dest_len = _strlen(dest);
+for (a = 0; a < n && src[a] != '\0'; a++)
+dest[dest_len + a] = src[a];
+return (dest);
+}
+
+/**
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *ptr;
+int size1, size2,a;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (a = 0; s1[a] != '\0'; a++)
+;
+size1 = a;
+for (a = 0; s2[a] != '\0'; a++)
+;
+size2 = a;
+ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+if (ptr == NULL)
+{
+return (NULL);
+}
+_strncat(ptr, s1, size1);
+_strncat(ptr, s2, size2);
+ptr += '\0';
+return (ptr);
+}
