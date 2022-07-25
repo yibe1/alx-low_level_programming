6e46ac08defd1948ba71b378a196de634931f3c9
yibe1 <yibe1481@gmail.com>
2022-07-25 19:04:14 +0300

    Update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - ..............
+ * @argc: The arg counter
+ * @argv: The arg values
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+int num, result = 0, i;
+while (argc-- > 1)
+{
+for (i = 0; argv[argc][i]; i++)
+{
+if (!(isdigit(argv[argc][i])))
+{
+printf("Error\n");
+return (1);
+}
+}
+num = atoi(argv[argc]);
+result += num;
+}
+printf("%d\n", result);
+return (0);
+}
