0a51fda3b3a7684cfa9a8f1050630ecd94d8da52
yibe1 <yibe1481@gmail.com>
2022-07-25 20:53:57 +0300

    update

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
+void _putchar(char r);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 #endif
