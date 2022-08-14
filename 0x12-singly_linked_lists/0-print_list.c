ebc044fa15c6c78438c7e739700dd9a9d5ad26a6
yibe1 <yibe1481@gmail.com>
2022-08-14 21:05:55 +0300

    linked list traversing function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,6 @@
  * Return: the size of the linked list
  */
 
-#include "lists.h"
-#include <stdio.h>
-
-/**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
- */
-
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
