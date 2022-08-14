187da7f864d6ad438f1a645e8f4675433fb9982a
yibe1 <yibe1481@gmail.com>
2022-08-14 18:33:23 +0300

    linked list traversing function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,51 +12,22 @@
  * Return: the size of the linked list
  */
 
-int print_list(const list_t *h){
+int print_list(const list_t *h)
+{
 
 const list_t *node;
 int i = 0;
 node = h;
-while(node != NULL){
-char *str = node -> str;
-printf("[%d] ",node -> len);
-if(str != NULL)
+while (node != NULL)
+{
+char *str = node->str;
+printf("[%d] ", node->len);
+if (str != NULL)
 printf("%s", str);
 node = node->next;
 printf("\n");
 i++;
 
 }
-return i;
+return (i);
 }
-/*int main(void)
-{
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    int n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
-}
-*/
