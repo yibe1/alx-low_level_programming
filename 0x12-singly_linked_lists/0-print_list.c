1620364f03fffd659f66f720de3471b57bd306ff
yibe1 <yibe1481@gmail.com>
2022-08-14 20:38:30 +0300

    linked list traversing function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,26 +10,21 @@
  * Description: a function that traverses through singly linked list
  * Return: the size of the linked list
  */
-int print_list(const list_t *h)
+size_t print_list(const list_t *h)
 {
-const list_t *node;
-int i = 0;
-node = h;
-while (node != NULL)
-{
-char *str = node->str;
-printf("[%d] ", node->len);
-if (str == NULL)
-{
-printf("(nil)");
-}
-else
-{
-printf("%s", str);
-}
-node = node->next;
-printf("\n");
-i++;
-}
-return (i);
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
