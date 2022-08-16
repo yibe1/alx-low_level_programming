0c371a55c4d4ad116d060968ef3b43a92882660a
yibe1 <yibe1481@gmail.com>
2022-08-16 21:17:32 +0300

    first submision of the function to count nodes in liniked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-size_t nnodes = 0;
+size_t size = 0;
 
 while (h != NULL)
 {
 h = h->next;
-nnodes++;
+size++;
 }
-return (nnodes);
+return (size);
 }
