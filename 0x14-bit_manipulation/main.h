9e681d4dc159ef7e5d1f0ba8ea88cb02f5f4302d
yibe1 <yibe1481@gmail.com>
2022-08-23 17:32:28 +0300

    update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ void print_binary(unsigned long int n);
 void _divide(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
