a4b52a30db7b467e0ac3f05c03bbc49cc81fdc31
yibe1 <yibe1481@gmail.com>
2022-08-25 17:24:45 +0300

    first file IO excersise

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,61 +4,69 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+
 /**
- * err_status - prints error
- * @msg: message to write
- * @file: file to write to
- * @exitstat: exit status given
- */
-void err_status(char *msg, char *file, int exitstat)
+  * main - Entry point
+  * @argc: The argument count
+  * @argv: The argument vector
+  *
+  * Return: ...
+  */
+int main(int argc, char **argv)
 {
-dprintf(STDERR_FILENO, "%s%s\n", msg, file);
-exit(exitstat);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
 }
+
+copy(argv[1], argv[2]);
+exit(0);
+}
+
 /**
- * main - copies to file two
- * @ac: number of arguments passed
- * @av: pointer to list of passed strings
- * Return: 1 on success
- */
-int main(int ac, char **av)
+  * copy - ...
+  * @src: ...
+  * @dest: ...
+  *
+  * Return: ...
+  */
+void copy(const char *src, const char *dest)
 {
-ssize_t file_d_write, file_d_read, read_bytes, write_bytes;
-int isbig = 1;
+int ofd, tfd, readed;
+char buff[1024];
 
-char buf[1024];
-
-if (ac != 3)
-err_status("Usage: cp file_from file_to", "", 97);
-
-file_d_write = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (file_d_write == -1)
-err_status("Error: Can't write to ", av[2], 99);
-
-file_d_read = open(av[1], O_RDONLY);
-if (file_d_read == -1)
-err_status("Error: Can't read from file ", av[1], 98);
-while (isbig)
+ofd = open(src, O_RDONLY);
+if (!src || ofd == -1)
 {
-read_bytes = read(file_d_read, buf, 1024);
-if (read_bytes == -1)
-err_status("Error: Can't read from file ", av[1], 98);
-write_bytes = write(file_d_write, buf, read_bytes);
-if (write_bytes == -1 || write_bytes != read_bytes)
-err_status("Error: Can't write to ", av[2], 99);
-
-if (write_bytes != 1024)
-isbig = 0;
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+exit(98);
 }
-if (close(file_d_read))
+
+tfd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+while ((readed = read(ofd, buff, 1024)) > 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int)file_d_read);
+if (write(tfd, buff, readed) != readed || tfd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+exit(99);
+}
+}
+
+if (readed == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+exit(98);
+}
+if (close(ofd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ofd);
 exit(100);
 }
-if (close(file_d_write))
+
+if (close(tfd) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (int)file_d_write);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfd);
 exit(100);
 }
-return (0);
 }
