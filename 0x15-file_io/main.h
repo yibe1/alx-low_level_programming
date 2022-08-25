489be58e1d731c7bb235b868d49a96c092c2e1cd
yibe1 <yibe1481@gmail.com>
2022-08-25 17:27:39 +0300

    first file IO excersise

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
-#define BUFSIZE 1024
-
-static ssize_t read_file(char *file, char **buf, int fd);
-static void write_copy(char *file, int fd, char *buf, int len);
+void copy(const char *src, const char *dest);
 
 #endif
